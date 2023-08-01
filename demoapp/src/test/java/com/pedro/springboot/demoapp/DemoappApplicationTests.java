51d3b16a48d61fb65c9354ffbbd2d1a6c7fc5586
Pedro <paf.victor@gmail.com>
2023-08-01 15:03:07 -0300

    First App - Spring Demo

---
+++ demoapp/src/test/java/com/pedro/springboot/demoapp/DemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.springboot.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
