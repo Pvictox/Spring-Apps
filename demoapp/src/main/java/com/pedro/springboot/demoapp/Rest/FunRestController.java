57ffe0c9fc54e979fa8cfe4f0a3546d601af4a3e
Pedro <paf.victor@gmail.com>
2023-08-02 14:21:50 -0300

    Working with project properties

--- demoapp/src/main/java/com/pedro/springboot/demoapp/Rest/FunRestController.java
+++ demoapp/src/main/java/com/pedro/springboot/demoapp/Rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.pedro.springboot.demoapp.Rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 //Set as a Rest Controller using @RestController annotation.
 @RestController
 public class FunRestController {
-    //@GetMapping it's a shortcut for @RequestMapping (method = RequestMethod.GET)
 
+    //Using Injection we can retrieve some info that was set on 'application.properties' file.
+    @Value("${coach.name}") //This annotation retrieves the info from the properties file
+    private String coachName;
+
+    //@GetMapping it's a shortcut for @RequestMapping (method = RequestMethod.GET)
     // Using an endpoint '/' who will return the message "Hello World"
 
    /* @GetMapping("/")
@@ -19,6 +24,6 @@ public class FunRestController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayHelloAgain(){
-        return "Hello World using RequestMapping";
+        return "Hello World using RequestMapping. Done by: "+this.coachName;
     }
 }
