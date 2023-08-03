08a3d0ec43d6dff665a41da4ccf27355b788c6de
Pedro <paf.victor@gmail.com>
2023-08-03 15:11:55 -0300

    Working with Actuator and Security

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/pedro/springboot/demoapp/Rest/FunRestController.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/pedro/springboot/demoapp/Rest/FunRestController.java
@@ -12,32 +12,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 
     //Using Injection we can retrieve some info that was set on 'application.properties' file.
-    @Value("${coach.name}") //This annotation retrieves the info from the properties file
+   /* @Value("${coach.name}") //This annotation retrieves the info from the properties file
     private String coachName;
     @Value("${team.name}")
     private String teamName;
     @Value("${app.version}")
-    private String appVersion;
+    private String appVersion;*/
 
 
     //@GetMapping it's a shortcut for @RequestMapping (method = RequestMethod.GET)
     // Using an endpoint '/' who will return the message "Hello World"
 
-   /* @GetMapping("/")
+    @GetMapping("/")
     public String sayHello(){
         return "Hello World";
-    }*/
+    }
 
     /*@RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayHelloAgain(){
         return "Hello World using RequestMapping. Done by: "+this.coachName;
     }*/
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    /*@RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayWelcomeMessage(){
         return "Welcome!\n"+"Here some details about this project: Team: "+this.teamName+" | Coach: "+this.coachName+
                 " | App Version: "+this.appVersion+"\n";
-    }
+    }*/
 
     @RequestMapping(value = "/workout", method = RequestMethod.GET)
     public String messageWorkout(){
