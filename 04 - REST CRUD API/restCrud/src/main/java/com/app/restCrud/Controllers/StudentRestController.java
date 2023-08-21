b32868d6d3103da5e294ddb511e75516d88023b1
Pedro <paf.victor@gmail.com>
2023-08-21 14:23:32 -0300

    Working with path variables

--- 04 - REST CRUD API/restCrud/src/main/java/com/app/restCrud/Controllers/StudentRestController.java
+++ 04 - REST CRUD API/restCrud/src/main/java/com/app/restCrud/Controllers/StudentRestController.java
@@ -1,6 +1,8 @@
 package com.app.restCrud.Controllers;
 
 import com.app.restCrud.Models.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> listStudent;
 
+    @PostConstruct
+    public void loadData(){
+        listStudent = new ArrayList<>();
+        listStudent.add(new Student("Pedro", "Fonseca"));
+        listStudent.add(new Student("Maria", "Ferraz"));
+        listStudent.add(new Student("Calors", "Maia"));
+    }
     @RequestMapping(method = RequestMethod.GET, value = "/allstudents")
     public List<Student> returnAllStudents(){
-        List<Student> list = new ArrayList<>();
-        list.add(new Student("Pedro", "Fonseca"));
-        list.add(new Student("Maria", "Ferraz"));
-        list.add(new Student("Calors", "Maia"));
-        return list;
+        return listStudent;
+    }
+
+    //Add endpoint /api/{studentId} where it will return the student with that ID.
+    @RequestMapping(value = "/{studentId}", method = RequestMethod.GET)
+    public Student getStudentById(@PathVariable int studentId){
+        return listStudent.get(studentId);
     }
 }
